ed3ecd57a9144fc44c811dedb62b0eb921ca3478
Pranay <pranays94@gmail.com>
2014-03-31 22:55:36 +0530

    Updated Transactions
    Removed Indices and `

--- Attendanceproj/tablecreation/t1.sql
+++ Attendanceproj/tablecreation/t1.sql
@@ -1,107 +1,89 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`login`
+-- Table    login 
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`login` (
-  `username` VARCHAR(30) NOT NULL ,
-  `password` VARCHAR(30) NULL ,
-  `usertype` VARCHAR(30) NULL ,
-  PRIMARY KEY (`username`) )
-ENGINE = InnoDB;
+CREATE  TABLE IF NOT EXISTS    login  (
+   username  VARCHAR(30) NOT NULL ,
+   password  VARCHAR(30) NULL ,
+   usertype  VARCHAR(30) NULL ,
+  PRIMARY KEY ( username ) );
+ 
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`teacher`
+-- Table    teacher 
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`teacher` (
-  `teacherid` INT NOT NULL ,
-  `teachername` VARCHAR(45) NULL ,
-  `Login_username` VARCHAR(30) NOT NULL ,
-  PRIMARY KEY (`teacherid`) ,
-  INDEX `fk_teacher_Login_idx` (`Login_username` ASC) ,
-  CONSTRAINT `fk_teacher_Login`
-    FOREIGN KEY (`Login_username` )
-    REFERENCES `mydb`.`login` (`username` )
+CREATE  TABLE IF NOT EXISTS    teacher  (
+   teacherid  INT NOT NULL ,
+   teachername  VARCHAR(45) NULL ,
+   Login_username  VARCHAR(30) NOT NULL ,
+  PRIMARY KEY ( teacherid ) ,
+  CONSTRAINT  fk_teacher_Login 
+    FOREIGN KEY ( Login_username  )
+    REFERENCES    login  ( username  )
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
+ 
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`subject`
+-- Table    subject 
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`subject` (
-  `subjectid` INT NOT NULL ,
-  `subjectname` VARCHAR(45) NULL ,
-  PRIMARY KEY (`subjectid`) )
-ENGINE = InnoDB;
+CREATE  TABLE IF NOT EXISTS    subject  (
+   subjectid  INT NOT NULL ,
+   subjectname  VARCHAR(45) NULL ,
+  PRIMARY KEY ( subjectid ) );
+ 
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`teaches`
+-- Table    teaches 
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`teaches` (
-  `teacher_teacherid` INT NOT NULL ,
-  `subject_idsubject` INT NOT NULL ,
-  PRIMARY KEY (`teacher_teacherid`, `subject_idsubject`) ,
-  INDEX `fk_teacher_has_subject_subject1_idx` (`subject_idsubject` ASC) ,
-  INDEX `fk_teacher_has_subject_teacher1_idx` (`teacher_teacherid` ASC) ,
-  CONSTRAINT `fk_teacher_has_subject_teacher1`
-    FOREIGN KEY (`teacher_teacherid` )
-    REFERENCES `mydb`.`teacher` (`teacherid` )
+CREATE  TABLE IF NOT EXISTS    teaches  (
+   teacher_teacherid  INT NOT NULL ,
+   subject_idsubject  INT NOT NULL ,
+  PRIMARY KEY ( teacher_teacherid ,  subject_idsubject ) ,
+  CONSTRAINT  fk_teacher_has_subject_teacher1 
+    FOREIGN KEY ( teacher_teacherid  )
+    REFERENCES    teacher  ( teacherid  )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_teacher_has_subject_subject1`
-    FOREIGN KEY (`subject_idsubject` )
-    REFERENCES `mydb`.`subject` (`subjectid` )
+  CONSTRAINT  fk_teacher_has_subject_subject1 
+    FOREIGN KEY ( subject_idsubject  )
+    REFERENCES    subject  ( subjectid  )
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
+ 
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`student`
+-- Table    student 
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`student` (
-  `idstudent` INT NOT NULL ,
-  `studentname` VARCHAR(45) NULL ,
-  `dept` VARCHAR(45) NULL ,
-  `semester` VARCHAR(45) NULL ,
-  `subject_subjectid` INT NOT NULL ,
-  PRIMARY KEY (`idstudent`, `subject_subjectid`) ,
-  INDEX `fk_student_subject1_idx` (`subject_subjectid` ASC) ,
-  CONSTRAINT `fk_student_subject1`
-    FOREIGN KEY (`subject_subjectid` )
-    REFERENCES `mydb`.`subject` (`subjectid` )
+CREATE  TABLE IF NOT EXISTS    student  (
+   idstudent  INT NOT NULL ,
+   studentname  VARCHAR(45) NULL ,
+   dept  VARCHAR(45) NULL ,
+   semester  VARCHAR(45) NULL ,
+   subject_subjectid  INT NOT NULL ,
+  PRIMARY KEY ( idstudent ,  subject_subjectid ) ,
+  CONSTRAINT  fk_student_subject1 
+    FOREIGN KEY ( subject_subjectid  )
+    REFERENCES    subject  ( subjectid  )
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
+ 
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`attendence`
+-- Table    attendence 
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`attendence` (
-  `date` DATE NULL ,
-  `status` VARCHAR(45) NULL ,
-  `student_idstudent` INT NOT NULL ,
-  `student_subject_subjectid` INT NOT NULL ,
-  INDEX `fk_attendence_student1_idx` (`student_idstudent` ASC, `student_subject_subjectid` ASC) ,
-  CONSTRAINT `fk_attendence_student1`
-    FOREIGN KEY (`student_idstudent` , `student_subject_subjectid` )
-    REFERENCES `mydb`.`student` (`idstudent` , `subject_subjectid` )
+CREATE  TABLE IF NOT EXISTS    attendence  (
+   date  DATE NULL ,
+   status  VARCHAR(45) NULL ,
+   student_idstudent  INT NOT NULL ,
+   student_subject_subjectid  INT NOT NULL ,
+  CONSTRAINT  fk_attendence_student1 
+    FOREIGN KEY ( student_idstudent  ,  student_subject_subjectid  )
+    REFERENCES    student  ( idstudent  ,  subject_subjectid  )
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+    ON UPDATE NO ACTION);
