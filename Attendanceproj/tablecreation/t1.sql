cab1c9f010b5de5c0de0c1b9e8a25124a40e1591
Pranay <pranays94@gmail.com>
2014-04-01 00:32:39 +0530

    FINAL TABLE CREATION STATEMENTS
    All statements working on Oracle by copy pasting

--- Attendanceproj/tablecreation/t1.sql
+++ Attendanceproj/tablecreation/t1.sql
@@ -2,7 +2,7 @@
 -- -----------------------------------------------------
 -- Table    login 
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS    login  (
+CREATE  TABLE     login  (
    username  VARCHAR(30) NOT NULL ,
    password  VARCHAR(30) NULL ,
    usertype  VARCHAR(30) NULL ,
@@ -13,7 +13,7 @@ CREATE  TABLE IF NOT EXISTS    login  (
 -- -----------------------------------------------------
 -- Table    teacher 
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS    teacher  (
+CREATE  TABLE     teacher  (
    teacherid  INT NOT NULL ,
    teachername  VARCHAR(45) NULL ,
    Login_username  VARCHAR(30) NOT NULL ,
@@ -21,15 +21,15 @@ CREATE  TABLE IF NOT EXISTS    teacher  (
   CONSTRAINT  fk_teacher_Login 
     FOREIGN KEY ( Login_username  )
     REFERENCES    login  ( username  )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON DELETE CASCADE
+   );
  
 
 
 -- -----------------------------------------------------
 -- Table    subject 
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS    subject  (
+CREATE  TABLE     subject  (
    subjectid  INT NOT NULL ,
    subjectname  VARCHAR(45) NULL ,
   PRIMARY KEY ( subjectid ) );
@@ -39,27 +39,27 @@ CREATE  TABLE IF NOT EXISTS    subject  (
 -- -----------------------------------------------------
 -- Table    teaches 
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS    teaches  (
+CREATE  TABLE     teaches  (
    teacher_teacherid  INT NOT NULL ,
    subject_idsubject  INT NOT NULL ,
   PRIMARY KEY ( teacher_teacherid ,  subject_idsubject ) ,
-  CONSTRAINT  fk_teacher_has_subject_teacher1 
+  CONSTRAINT  fk_teacher_teacher1 
     FOREIGN KEY ( teacher_teacherid  )
     REFERENCES    teacher  ( teacherid  )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT  fk_teacher_has_subject_subject1 
+    ON DELETE CASCADE
+   ,
+  CONSTRAINT  fk_teacher_subject1 
     FOREIGN KEY ( subject_idsubject  )
     REFERENCES    subject  ( subjectid  )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON DELETE CASCADE
+   );
  
 
 
 -- -----------------------------------------------------
 -- Table    student 
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS    student  (
+CREATE  TABLE     student  (
    idstudent  INT NOT NULL ,
    studentname  VARCHAR(45) NULL ,
    dept  VARCHAR(45) NULL ,
@@ -69,21 +69,21 @@ CREATE  TABLE IF NOT EXISTS    student  (
   CONSTRAINT  fk_student_subject1 
     FOREIGN KEY ( subject_subjectid  )
     REFERENCES    subject  ( subjectid  )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON DELETE CASCADE
+   );
  
 
 
 -- -----------------------------------------------------
 -- Table    attendence 
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS    attendence  (
-   date  DATE NULL ,
+CREATE  TABLE     attendence  (
+   date1  DATE NULL ,
    status  VARCHAR(45) NULL ,
    student_idstudent  INT NOT NULL ,
    student_subject_subjectid  INT NOT NULL ,
   CONSTRAINT  fk_attendence_student1 
     FOREIGN KEY ( student_idstudent  ,  student_subject_subjectid  )
     REFERENCES    student  ( idstudent  ,  subject_subjectid  )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON DELETE CASCADE
+   );
